14549dfb9cea3b89ebe6b575956f6485d2751f43
Ryan Berlin <ryanaberlin@gmail.com>
2018-09-13 12:42:33 -0400

    Set up tcp connection

--- toy_des_cli.py
+++ toy_des_cli.py
@@ -1,12 +1,18 @@
 import sys
 import socket
+import random
 
-#read user input
+#read user input and set up connections
 def getCommand():
 	#Prompt for command
-	command = raw_input("SEND or LISTEN? ")
+	command = raw_input("SEND or RECEIVE? ")
 
-	if command == "LISTEN":
+	if command == "RECEIVE":
+		#listen for connections
+		port = int(random.random()*64511 + 1024)
+		sock.bind(("", socket.htons(port)))
+		sock.listen(1)
+		print "Listening for connections on", port
 		recvFile()
 	elif command == "SEND":
 		#get the file
@@ -14,7 +20,7 @@ def getCommand():
 		try:
 			file = open(filename)
 		except:
-			print "Could not open file: ", filename
+			print "Could not open file:", filename
 			getCommand()
 
 		#Get address
@@ -22,23 +28,42 @@ def getCommand():
 		port = int(input("Port: "))
 
 		#attempt to connect
-		sock.connect((ip, socket.htons(port)))
+		try:
+			sock.connect((ip, socket.htons(port)))
+		except:
+			"Unable to connect"
+			getCommand()
 
-		sendFile()
+		sendFile(file)
 	else:
 		print "Invalid Command"
 		getCommand()
 
 #sets up a connection, encrypts the file and sends to the
 #given address
-def sendFile(file, ip, port):
-	print "send"
+def sendFile(file):
+	data = None
+	while data != "":
+		data = file.read(1024)
+		sock.sendall(data)
 
 #listens on a port for a connection
 #receives a file, decrypts and saves.
 def recvFile():
-	print "recv"
+	#wait for connection
+	fd, addr = sock.accept()
+	print "Conncetion received from", addr
+
+	data = None
+	while data != "":
+		data = fd.recv(1024)
+		print data
+
+	fd.close()
+
+
 
 ###MAIN###
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 getCommand()
+sock.close()
