f6f63177ce9fb21dcd9b339a92bfe1ac6d3d6054
Ryan Berlin <ryanaberlin@gmail.com>
2018-09-17 00:13:26 -0400

    Added key as user input

--- toy_des_cli.py
+++ toy_des_cli.py
@@ -20,8 +20,21 @@ def getBytes(binary):
 		hexi = "0" + hexi
 	return bytes.fromhex(hexi)
 
+def checkKey(key):
+	if len(key) != 10:
+		return False
+	for b in key:
+		if b != "0" and b != "1":
+			return False
+	return True 
+
 #read user input and set up connections
 def getCommand():
+	#Prompt for key
+	key = input("10-bit Key: ")
+	if not checkKey(key):
+		print("invalid key")
+		return
 	#Prompt for command
 	command = input("SEND or RECEIVE? ")
 
@@ -31,7 +44,7 @@ def getCommand():
 		sock.bind(("", socket.htons(port)))
 		sock.listen(1)
 		print("Listening for connections on", port)
-		recvFile()
+		recvFile(key)
 	elif command == "SEND":
 		#get the file
 		filename = input("file: ")
@@ -39,7 +52,7 @@ def getCommand():
 			file = open(filename, "rb")
 		except:
 			print ("Could not open file:", filename)
-			getCommand()
+			return
 
 		#Get address
 		ip = input("IP: ")
@@ -49,17 +62,17 @@ def getCommand():
 		try:
 			sock.connect((ip, socket.htons(port)))
 		except:
-			"Unable to connect"
-			getCommand()
+			print("Unable to connect")
+			return
 
-		sendFile(file)
+		sendFile(file, key)
 	else:
 		print("Invalid Command")
-		getCommand()
+		return
 
 #sets up a connection, encrypts the file and sends to the
 #given address
-def sendFile(file):
+def sendFile(file, key):
 	#Send name of file first
 	#ENCRYPT
 	cypher = des.encrypt(getBits(bytes(file.name, "utf-8")), key)
@@ -68,8 +81,7 @@ def sendFile(file):
 	#Wait for acknowledgement
 	ack = sock.recv(1024)
 	if ack != b'ACK': 
-		print("yh")
-		return
+		sendFile(file, key)
 
 	#Send the rest
 	data = file.read(1024)
@@ -82,7 +94,7 @@ def sendFile(file):
 
 #listens on a port for a connection
 #receives a file, decrypts and saves.
-def recvFile():
+def recvFile(key):
 	#wait for connection
 	fd, addr = sock.accept()
 	print("Conncetion received from", addr)
@@ -92,7 +104,7 @@ def recvFile():
 	#DECRYPT
 	filename = getBytes(des.decrypt(getBits(cypher), key))
 	#open the file
-	file = open(filename, "ab")
+	file = open(filename, "wb")
 
 	#Send Acknowledgement
 	fd.sendall(b'ACK')
@@ -105,11 +117,10 @@ def recvFile():
 		cypher = fd.recv(1024)
 
 	fd.close()
-	print("Received file:", filename)
+	print("Received file:", filename.decode())
 
 
 ###MAIN###
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-key = "1100100110"
 getCommand()
 sock.close()
